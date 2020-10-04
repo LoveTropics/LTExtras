449d9fcd9e95568ef492e8da971bbc8593aaab85
tterrag <tterrag1098@gmail.com>
2020-10-04 14:23:10 -0400

    Fix fake water blocks being selectable in survival

--- src/main/java/com/lovetropics/extras/block/FakeWaterBlock.java
+++ src/main/java/com/lovetropics/extras/block/FakeWaterBlock.java
@@ -1,16 +1,13 @@
 package com.lovetropics.extras.block;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Block.Properties;
-import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class FakeWaterBlock extends WaterBarrierBlock {
 
@@ -19,26 +16,19 @@ public class FakeWaterBlock extends WaterBarrierBlock {
 	}
 
 	@Override
-	public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-		return VoxelShapes.empty();
+	@Deprecated
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		if (worldIn instanceof World && ((World) worldIn).isRemote) {
+			if (context.getEntity() instanceof PlayerEntity && !((PlayerEntity)context.getEntity()).isCreative()) {
+				return VoxelShapes.empty();
+			}
+		}
+
+		return super.getShape(state, worldIn, pos, context);
 	}
 
 	@Override
-	@Deprecated
-	public VoxelShape getRaytraceShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
-		if (worldIn instanceof World && ((World)worldIn).isRemote) {
-			return getClientRaytraceShape(state, worldIn, pos);
-		} else {
-			return super.getRaytraceShape(state, worldIn, pos);
-		}
-	}
-
-	@OnlyIn(Dist.CLIENT)
-	private VoxelShape getClientRaytraceShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
-		if (Minecraft.getInstance().player == null || Minecraft.getInstance().player.isCreative()) {
-			return super.getRaytraceShape(state, worldIn, pos);
-		} else {
-			return VoxelShapes.empty();
-		}
+	public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return VoxelShapes.empty();
 	}
 }
