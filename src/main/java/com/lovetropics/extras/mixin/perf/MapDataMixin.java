b9ad1e05c9acd6e8306f5d1ab02694b14d6aad58
gegy1000 <gegy1000@gmail.com>
2020-11-14 01:51:20 +0200

    only disable decorations

--- src/main/java/com/lovetropics/extras/mixin/perf/MapDataMixin.java
+++ src/main/java/com/lovetropics/extras/mixin/perf/MapDataMixin.java
@@ -1,16 +1,31 @@
 package com.lovetropics.extras.mixin.perf;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.storage.MapData;
+import net.minecraft.world.storage.MapDecoration;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MapData.class)
 public class MapDataMixin {
 
 	// NUKE
-	@Overwrite
-	public void updateVisiblePlayers(PlayerEntity player, ItemStack mapStack) {}
+	@Redirect(
+			method = "updateVisiblePlayers",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;hasItemStack(Lnet/minecraft/item/ItemStack;)Z")
+	)
+	public boolean searchForMap(PlayerInventory playerInventory, ItemStack stack) {
+		return false;
+	}
+
+	@Redirect(
+			method = "updateVisiblePlayers",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/storage/MapData;updateDecorations(Lnet/minecraft/world/storage/MapDecoration$Type;Lnet/minecraft/world/IWorld;Ljava/lang/String;DDDLnet/minecraft/util/text/ITextComponent;)V")
+	)
+	public void updateDecoration(MapData mapData, MapDecoration.Type type, IWorld worldIn, String decorationName, double worldX, double worldZ, double rotationIn, ITextComponent p_191095_10_) {
+	}
 }
