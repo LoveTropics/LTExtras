946c7e7e4f199388d3cbd91cb1c16ccf3ab705ed
gegy1000 <gegy1000@gmail.com>
2020-11-14 16:47:11 +0200

    print extra info

---
+++ src/main/java/com/lovetropics/extras/mixin/NetworkSystemMixin.java
@@ -0,0 +1,22 @@
+package com.lovetropics.extras.mixin;
+
+import net.minecraft.network.NetworkSystem;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(NetworkSystem.class)
+public class NetworkSystemMixin {
+	@Redirect(
+			method = "tick",
+			at = @At(
+					value = "INVOKE",
+					target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V",
+					remap = false
+			)
+	)
+	private static void warn(Logger logger, String message, Object p0, Object p1) {
+		((Exception) p1).printStackTrace();
+	}
+}
