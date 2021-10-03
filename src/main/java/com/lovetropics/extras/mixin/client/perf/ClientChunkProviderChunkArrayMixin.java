6f4b13f3f2aea7a47999e26c7965b07654322436
Gegy <gegy1000@gmail.com>
2021-10-03 23:21:37 +0200

    reinitialize chunk array due to erroring redirect injector

--- src/main/java/com/lovetropics/extras/mixin/client/perf/ClientChunkProviderChunkArrayMixin.java
+++ src/main/java/com/lovetropics/extras/mixin/client/perf/ClientChunkProviderChunkArrayMixin.java
@@ -1,19 +1,23 @@
 package com.lovetropics.extras.mixin.client.perf;
 
+import net.minecraft.client.multiplayer.ClientChunkProvider;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.chunk.Chunk;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.concurrent.atomic.AtomicReferenceArray;
 
 @Mixin(targets = "net/minecraft/client/multiplayer/ClientChunkProvider$ChunkArray")
-public class ClientChunkProviderMixin {
+public class ClientChunkProviderChunkArrayMixin {
+    @Shadow @Final @Mutable private AtomicReferenceArray<Chunk> chunks;
     @Shadow @Final private int sideLength;
 
     @Unique
@@ -21,12 +25,12 @@ public class ClientChunkProviderMixin {
     @Unique
     private int tableShift;
 
-    @Redirect(method = "<init>", at = @At(value = "NEW", target = "Ljava/util/concurrent/atomic/AtomicReferenceArray;<init>(I)V", remap = false))
-    private AtomicReferenceArray<Chunk> createChunkArray(int length) {
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void init(ClientChunkProvider chunkProvider, int viewDistance, CallbackInfo ci) {
         int tableSize = MathHelper.smallestEncompassingPowerOfTwo(this.sideLength);
         this.tableMask = tableSize - 1;
         this.tableShift = MathHelper.log2(tableSize);
-        return new AtomicReferenceArray<>(tableSize * tableSize);
+        this.chunks = new AtomicReferenceArray<>(tableSize * tableSize);
     }
 
     /**
