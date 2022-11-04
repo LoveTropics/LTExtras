e7bec3c5c4c929bc2df9e539d47b112ab7402a07
Gegy <gegy.dev@gmail.com>
2022-11-04 22:38:44 +0100

    Fish eye for non-players

--- src/main/java/com/lovetropics/extras/mixin/LivingEntityMixin.java
+++ src/main/java/com/lovetropics/extras/mixin/LivingEntityMixin.java
@@ -1,11 +1,17 @@
 package com.lovetropics.extras.mixin;
 
 import com.lovetropics.extras.ExtraBlocks;
+import com.lovetropics.extras.effect.ExtraEffects;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.material.Fluid;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
@@ -15,11 +21,30 @@ public abstract class LivingEntityMixin extends Entity {
 		super(type, level);
 	}
 
+	@Shadow
+	public abstract boolean hasEffect(MobEffect effect);
+
 	@ModifyConstant(method = "handleRelativeFrictionAndCalculateMovement", constant = @Constant(doubleValue = 0.2))
 	private double modifyClimbSpeed(double speed) {
 		if (getFeetBlockState().is(ExtraBlocks.PARACORD.get())) {
 			return speed * 2.0;
 		}
 		return speed;
+	}
+
+	@Override
+	public boolean updateFluidHeightAndDoFluidPushing(TagKey<Fluid> fluid, double scale) {
+		if (fluid == FluidTags.WATER && hasEffect(ExtraEffects.FISH_EYE.get())) {
+			return false;
+		}
+		return super.updateFluidHeightAndDoFluidPushing(fluid, scale);
+	}
+
+	@Override
+	public boolean isEyeInFluid(TagKey<Fluid> fluid) {
+		if (fluid == FluidTags.WATER && hasEffect(ExtraEffects.FISH_EYE.get())) {
+			return false;
+		}
+		return super.isEyeInFluid(fluid);
 	}
 }
