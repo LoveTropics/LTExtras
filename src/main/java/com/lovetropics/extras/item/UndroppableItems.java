30723da59238a0f64f0ee8cef78d9597899aef91
Gegy <gegy.dev@gmail.com>
2023-10-25 00:02:29 +0200

    Add undroppable tag

---
+++ src/main/java/com/lovetropics/extras/item/UndroppableItems.java
@@ -0,0 +1,40 @@
+package com.lovetropics.extras.item;
+
+import com.lovetropics.extras.LTExtras;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.entity.item.ItemTossEvent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = LTExtras.MODID)
+public class UndroppableItems {
+    private static final String TAG_UNDROPPABLE = "undroppable";
+
+    @SubscribeEvent
+    public static void onItemToss(final ItemTossEvent event) {
+        if (isUndroppable(event.getEntity().getItem())) {
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onItemPickup(final EntityItemPickupEvent event) {
+        final ItemStack stack = event.getItem().getItem();
+        if (isUndroppable(stack)) {
+            stack.setCount(0);
+            event.getItem().discard();
+            event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLivingDrops(final LivingDropsEvent event) {
+        event.getDrops().removeIf(item -> isUndroppable(item.getItem()));
+    }
+
+    private static boolean isUndroppable(final ItemStack stack) {
+        return stack.getTag() != null && stack.getTag().contains(TAG_UNDROPPABLE);
+    }
+}
