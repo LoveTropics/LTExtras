9202075c0299ff3de0dd335d504848ca1110cc73
Gegy <gegy.dev@gmail.com>
2023-10-26 19:11:58 +0200

    commit messages have lost all meaning

--- src/main/java/com/lovetropics/extras/mixin/collectible/ItemStackMixin.java
+++ src/main/java/com/lovetropics/extras/mixin/collectible/ItemStackMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ItemStack.class)
 public class ItemStackMixin {
-    @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResultHolder;"), remap = false)
+    @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResultHolder;"))
     private InteractionResultHolder<ItemStack> useItem(final Item item, final Level level, final Player player, final InteractionHand hand) {
         return CollectibleItemBehavior.wrapUse((ItemStack) (Object) this, level, player, hand);
     }
