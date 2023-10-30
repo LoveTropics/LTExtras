c889cbcf6d9d2120a8826898844e4a21ab92db37
Gegy <gegy.dev@gmail.com>
2023-10-30 17:37:18 +0100

    Fix: make sure we don't lose time zone when transferring into minigames

--- src/main/java/com/lovetropics/extras/schedule/TimeZoneSender.java
+++ src/main/java/com/lovetropics/extras/schedule/TimeZoneSender.java
@@ -14,6 +14,16 @@ import java.time.ZoneId;
 public class TimeZoneSender {
     @SubscribeEvent
     public static void onLogIn(final ClientPlayerNetworkEvent.LoggingIn event) {
+        sendTimeZoneToServer();
+    }
+
+    // When the server recreates the player, we can't be sure that it kept our time zone - send it again to be sure
+    @SubscribeEvent
+    public static void onPlayerClone(final ClientPlayerNetworkEvent.Clone event) {
+        sendTimeZoneToServer();
+    }
+
+    private static void sendTimeZoneToServer() {
         LTExtrasNetwork.CHANNEL.sendToServer(new SetTimeZonePacket(ZoneId.systemDefault()));
     }
 }
