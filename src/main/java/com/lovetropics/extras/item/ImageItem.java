b0861b7a4b8230ab5846ac8b9adda3248d6f133e
Gegy <gegy.dev@gmail.com>
2023-10-31 14:55:06 +0100

    Add new images

--- src/main/java/com/lovetropics/extras/item/ImageItem.java
+++ src/main/java/com/lovetropics/extras/item/ImageItem.java
@@ -15,7 +15,14 @@ import java.util.List;
 public class ImageItem extends Item {
     public static final List<ImageData> PRESETS = List.of(
             new ImageData(Component.literal("[Redacted]"), new ResourceLocation(LTExtras.MODID, "textures/images/ccfucc_banner.png"), 3.0f, 5.0f),
-            new ImageData(Component.literal("Plantain Pano 50"), new ResourceLocation(LTExtras.MODID, "textures/images/plantain_pano_50.png"), 8.73f, 2.0f)
+            new ImageData(Component.literal("Panorama: Plantain"), new ResourceLocation(LTExtras.MODID, "textures/images/plantain_pano_50.png"), 8.73f, 2.0f),
+            new ImageData(Component.literal("Compost"), new ResourceLocation(LTExtras.MODID, "textures/images/compost.png"), 2.664f, 2.0f),
+            new ImageData(Component.literal("Cook Stove Top"), new ResourceLocation(LTExtras.MODID, "textures/images/cook_stove_top_r.png"), 2.64f, 2.0f),
+            new ImageData(Component.literal("Group R"), new ResourceLocation(LTExtras.MODID, "textures/images/group_r.png"), 2.67f, 2.0f),
+            new ImageData(Component.literal("Jocote"), new ResourceLocation(LTExtras.MODID, "textures/images/jocote.png"), 3.0f, 2.0f),
+            new ImageData(Component.literal("Making Organic Fungicide"), new ResourceLocation(LTExtras.MODID, "textures/images/making_org_fung.png"), 2.672f, 2.0f),
+            new ImageData(Component.literal("Panorama: Garden Bike"), new ResourceLocation(LTExtras.MODID, "textures/images/pano_garden_bike.png"), 2.63f, 1.0f),
+            new ImageData(Component.literal("Plantain Ent"), new ResourceLocation(LTExtras.MODID, "textures/images/plantain_ent.png"), 2.5f, 3.0f)
     );
 
     public ImageItem(final Properties properties) {
