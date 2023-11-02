b5ccd51c11db37d498f5b670326d9c30c72efa6d
Gegy <gegy.dev@gmail.com>
2023-11-02 17:58:45 +0100

    Fix: also give collectible when you shift-click result out from Villager

--- src/main/java/com/lovetropics/extras/mixin/collectible/MerchantResultSlotMixin.java
+++ src/main/java/com/lovetropics/extras/mixin/collectible/MerchantResultSlotMixin.java
@@ -5,15 +5,21 @@ import com.lovetropics.extras.collectible.CollectibleStore;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.MerchantResultSlot;
 import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MerchantResultSlot.class)
 public class MerchantResultSlotMixin {
-    @Inject(method = "onTake", at = @At("HEAD"))
-    private void onTake(final Player player, final ItemStack stack, final CallbackInfo ci) {
+    @Shadow
+    @Final
+    private Player player;
+
+    @Inject(method = "checkTakeAchievements", at = @At("HEAD"))
+    private void onTake(final ItemStack stack, final CallbackInfo ci) {
         final Collectible collectible = Collectible.byItem(stack);
         if (collectible == null) {
             return;
