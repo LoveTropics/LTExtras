5aca161694f796093b5e40a516098d4dbbdac133
Gegy <gegy.dev@gmail.com>
2024-10-20 18:28:38 +0200

    Add Speedy Chunk block

--- src/main/java/com/lovetropics/extras/ExtraBlocks.java
+++ src/main/java/com/lovetropics/extras/ExtraBlocks.java
@@ -527,7 +527,8 @@ public class ExtraBlocks {
 			.add(Blocks.SPRUCE_SLAB, SpeedyBlock::slab)
 			.add(Blocks.PACKED_MUD, SpeedyBlock::opaque)
 			.add(Blocks.MUD_BRICKS, SpeedyBlock::opaque)
-			.add(Blocks.MUD_BRICK_SLAB, SpeedyBlock::opaque);
+			.add(Blocks.MUD_BRICK_SLAB, SpeedyBlock::opaque)
+			.add(ResourceLocation.fromNamespaceAndPath("tropicraft", "chunk"), SpeedyBlock::opaque);
 
 	public static final Map<Holder<Block>, BlockEntry<? extends SpeedyBlock>> SPEEDY_BLOCKS = SPEEDY_BLOCK_TEMPLATES
 			.build((object, factory) -> REGISTRATE
