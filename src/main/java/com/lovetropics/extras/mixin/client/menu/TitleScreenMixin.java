6f8059d1ec64bf0c87edfddfffcc09e163a936ad
Gegy <gegy.dev@gmail.com>
2024-11-16 05:18:23 +0100

    Fix: banish Realms from the Realm again

--- src/main/java/com/lovetropics/extras/mixin/client/menu/TitleScreenMixin.java
+++ src/main/java/com/lovetropics/extras/mixin/client/menu/TitleScreenMixin.java
@@ -1,16 +1,24 @@
 package com.lovetropics.extras.mixin.client.menu;
 
+import com.mojang.realmsclient.gui.screens.RealmsNotificationsScreen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nullable;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
-	@Inject(method = "realmsNotificationsEnabled", at = @At("HEAD"), cancellable = true)
-	private void realmsNotificationsEnabled(CallbackInfoReturnable<Boolean> cir) {
+	@Shadow
+	@Nullable
+	private RealmsNotificationsScreen realmsNotificationsScreen;
+
+	@Inject(method = "init", at = @At("TAIL"))
+	private void postInit(CallbackInfo ci) {
 		// Realms button is gone, so that's not useful
-		cir.setReturnValue(false);
+		realmsNotificationsScreen = null;
 	}
 }
