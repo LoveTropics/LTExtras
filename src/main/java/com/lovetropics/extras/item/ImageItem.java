84842397dfef5c4b7270791cd576010dc99799c6
Gegy <gegy.dev@gmail.com>
2024-11-18 14:22:07 +0100

    Add images for education area

--- src/main/java/com/lovetropics/extras/item/ImageItem.java
+++ src/main/java/com/lovetropics/extras/item/ImageItem.java
@@ -15,8 +15,24 @@ public class ImageItem extends Item {
             new ImageData(Component.literal("[Redacted]"), LTExtras.location("textures/images/ccfucc_banner.png"), 3.0f, 5.0f),
             new ImageData(Component.literal("Plantain Ent"), LTExtras.location("textures/images/plantain_ent.png"), 2.5f, 3.0f),
             new ImageData(Component.literal("Manatee"), LTExtras.location("textures/images/manatee.png"), 2.5f, 3.0f),
-            new ImageData(Component.literal("Shoebill"), LTExtras.location("textures/images/shoebill.png"), 2.5f, 3.0f)
+            new ImageData(Component.literal("Shoebill"), LTExtras.location("textures/images/shoebill.png"), 2.5f, 3.0f),
+            imageWithHeight("Papyrus Canary", "canary_papyrus", 3.0f, 256, 335),
+            imageWithHeight("Habitat Restoration - Riverbank", "habitat_restoration_riverbank_1", 3.0f, 256, 340),
+            imageWithWidth("Papyrus Gonolek", "papyrus_gonolek", 2.0f, 266, 200),
+            imageWithHeight("Papyrus Harvest", "papyrus_harvest", 2.0f, 340, 256),
+            imageWithWidth("Shoebill Stork", "shoebill_stork", 3.0f, 317, 256),
+            imageWithWidth("White-Winged Warbler", "white_winged_warbler", 3.0f, 301, 200)
     );
+
+    private static ImageData imageWithWidth(String name, String path, float blockWidth, int textureWidth, int textureHeight) {
+        float blockHeight = blockWidth / textureWidth * textureHeight;
+        return new ImageData(Component.literal(name), LTExtras.location("textures/images/" + path + ".png"), blockWidth, blockHeight);
+    }
+
+    private static ImageData imageWithHeight(String name, String path, float blockHeight, int textureWidth, int textureHeight) {
+        float blockWidth = blockHeight / textureHeight * textureWidth;
+        return new ImageData(Component.literal(name), LTExtras.location("textures/images/" + path + ".png"), blockWidth, blockHeight);
+    }
 
     public ImageItem(Properties properties) {
         super(properties);
