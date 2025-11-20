7dc72e856b0d11a167fffbacdf027d3072ac0785
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2025-11-20 11:19:31 -0600

    Make checkpoints water loggable

--- src/main/java/com/lovetropics/extras/block/CheckpointBlock.java
+++ src/main/java/com/lovetropics/extras/block/CheckpointBlock.java
@@ -6,27 +6,39 @@ import com.mojang.serialization.MapCodec;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.ScheduledTickAccess;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition.Builder;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class CheckpointBlock extends Block {
+public class CheckpointBlock extends Block implements SimpleWaterloggedBlock {
     public static final MapCodec<CheckpointBlock> CODEC = simpleCodec(CheckpointBlock::new);
 
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     public static final IntegerProperty STAGE = IntegerProperty.create("stage", 1, 40);
 
     public CheckpointBlock(Properties properties) {
         super(properties);
-        registerDefaultState(defaultBlockState().setValue(STAGE, 1));
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(STAGE, 1)
+                .setValue(WATERLOGGED, false));
     }
 
     @Override
@@ -35,8 +47,29 @@ public class CheckpointBlock extends Block {
     }
 
     @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        FluidState fluid = context.getLevel().getFluidState(context.getClickedPos());
+        boolean waterlogged = fluid.getType() == Fluids.WATER;
+        return super.getStateForPlacement(context).setValue(WATERLOGGED, waterlogged);
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    @Override
+    protected BlockState updateShape(BlockState state, LevelReader level, ScheduledTickAccess scheduledTickAccess, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, RandomSource random) {
+        if (state.getValue(WATERLOGGED)) {
+            scheduledTickAccess.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        }
+
+        return super.updateShape(state, level, scheduledTickAccess, pos, direction, neighborPos, neighborState, random);
+    }
+
+    @Override
     protected void createBlockStateDefinition(Builder<Block, BlockState> builder) {
-        builder.add(STAGE);
+        builder.add(STAGE, WATERLOGGED);
     }
 
     @Override
